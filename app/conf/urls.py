c80879ec54634032a345ae114a80c806a127aa2d
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-10-24 00:24:30 +0300

    Add djanx.base app with initial configuration and URL routing

--- app/conf/urls.py
+++ app/conf/urls.py
@@ -21,6 +21,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("djanx/", include("djanx.urls")),
     path("", include("app.home.urls")),
 ]
 
