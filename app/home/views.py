f949b6d46a49d3f14f27cb3c3a3ed214b13cb379
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-10-24 23:17:23 +0300

    Add page_title to HomeView and remove hardcoded title in base layout

--- app/home/views.py
+++ app/home/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = "home/index.html"
+    extra_context = {
+        "page_title": "The Spark Playhouse",
+    }
