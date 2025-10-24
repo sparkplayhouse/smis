b41cdf1322c95bc3a8441996157587cb2e9fa4dd
Kevin Wasike Wakhisi <kevin@tawalabora.space>
2025-10-24 03:35:11 +0300

    Refactor settings: reorganize local and production configurations for clarity and security

--- app/conf/local.py
+++ app/conf/local.py
@@ -1,5 +1,13 @@
-from split_settings.tools import include
 from pathlib import Path
+from split_settings.tools import include
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-f+nixt7zjkqi9l*ju5(kpv$e!+f@01b_b(#90rbzjpr0e&vr-i"
+
+ALLOWED_HOSTS = []
 
 # Include settings segments
 SPLIT = Path(__file__).parent / "settings"
@@ -8,11 +16,3 @@ include(
     str(SPLIT / "main.py"),  # Load main settings first
     str(SPLIT / "assets.py"),
 )
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-f+nixt7zjkqi9l*ju5(kpv$e!+f@01b_b(#90rbzjpr0e&vr-i"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
--- app/conf/production.py
+++ app/conf/production.py
@@ -1,8 +1,16 @@
+"""See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/"""
+
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 from split_settings.tools import include, optional
 
-# See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config("SECRET_KEY")
+
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv())
 
 # Include settings segments
 SPLIT = Path(__file__).parent / "settings"
@@ -11,11 +19,3 @@ include(
     str(SPLIT / "main.py"),  # Load main settings first
     str(SPLIT / "assets.py"),
 )
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "www.thesparkplayhouse.info"]
